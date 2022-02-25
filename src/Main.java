7406bbcce674eefb789e20809c5fbfe2edd0819f
aRinak <grisina047@gmail.com>
2022-02-25 13:51:33 +0300

    fix: Считает бонусные мили

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int saleTicket = 1500;
+        int totalMile = saleTicket / 20;
+        System.out.println(totalMile);
+    }
+}
